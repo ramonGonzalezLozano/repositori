fc89a3c9a6bb4dedd0fef14edc7984dd9eff9058
ricky <ramon.katxoperro@gmail.com>
2015-11-09 16:32:04 +0100

    missatge de confirmació

---
+++ src/ramongonzalezlozano/RamonGonzalezLozano.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramongonzalezlozano;
+
+/**
+ *
+ * @author ricky
+ */
+public class RamonGonzalezLozano {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private String nom2 = "Ricky";
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
